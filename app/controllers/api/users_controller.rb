cfb05e751f898e2dff9e715e103b687193cef4a2
snakerabbit <alison.s.cheng@gmail.com>
2018-01-23 11:14:35 -0800

    added user controller methods

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,43 @@
 class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render 'api/users/show'
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+
+  def index
+    @users = User.all.where("username != ?", current_user.username)
+    if @users
+      render :index
+    else
+      render json: @users.errors, status: 422
+    end
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      render :show
+    else
+      render json: @user.errors, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :instrument, :image_url, :about_me, :fave_bands, :fave_genres, :purpose)
+  end
 end
