2d932e48f99e043a28b429538db7e21fb0b1a02d
snakerabbit <alison.s.cheng@gmail.com>
2018-01-23 11:06:43 -0800

    added columns to user model

--- db/migrate/20180123185254_create_users.rb
+++ db/migrate/20180123185254_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :password_digest
-      t.string :session_token
-      t.string :email
-      t.string :img_url
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.string :email, null: false
+      t.string :img_url, default: 'https://cdn1.iconfinder.com/data/icons/ui-5/502/profile-512.png'
 
       t.timestamps
     end
