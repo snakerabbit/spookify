913dc416af65b86dea771d0bbf98601b8ee9ea6d
snakerabbit <alison.s.cheng@gmail.com>
2018-01-24 18:42:46 -0800

    added routes to login and signup pages

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import SessionFormContainer from './session_form/session_form_container';
 
 const App = () => (
   <div>
     <h1>Spookify</h1>
+    <Route path='/login' component={SessionFormContainer}></Route>
+    <Route path='/signup' component={SessionFormContainer}></Route>
   </div>
 );
 
