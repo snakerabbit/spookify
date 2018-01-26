45d4f5bdab2a99359a659623c38d562672da02ae
snakerabbit <alison.s.cheng@gmail.com>
2018-01-25 18:33:12 -0800

    added create and destroy methods

--- app/controllers/api/user_follows_controller.rb
+++ app/controllers/api/user_follows_controller.rb
@@ -1,2 +1,26 @@
 class Api::UserFollowsController < ApplicationController
+
+  def create
+    @userfollow = UserFollow.new
+    @userfollow.followed_user_id = params[:followed_user_id]
+    @userfollow.user_id = current_user.id
+
+    if @userfollow.save
+      @user = User.find_by(id: :followed_user_id)
+      render "api/users/show", user: @user
+    else
+      render json: @userfollow.errors.full_messages
+    end
+  end
+
+  def destroy
+    @userfollow = User.find_by(followed_user_id: params[:id])
+    @userfollow.user_id = current_user.id
+    @userfollow.destroy!
+
+    @user = User.find_by(id: params[:id])
+    render "api/users/show", user: @user
+  end
+
+
 end
